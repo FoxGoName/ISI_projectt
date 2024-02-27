b8c1e7406d4d8fe8000946b78a3a6a9b643a95fe
FoxGoName <106571781+FoxGoName@users.noreply.github.com>
2024-02-27 16:17:16 +0800

    v6 finish

--- ISI/apps/cart/cart.py
+++ ISI/apps/cart/cart.py
@@ -58,3 +58,6 @@ class Cart(object):
         print('save')
         self.session[settings.CART_SESSION_ID] = self.cart
         self.sessionmodfield = True
+
+    def get_total_length(self):
+        return sum(int(item['quantity']) for item in self.cart.values())
